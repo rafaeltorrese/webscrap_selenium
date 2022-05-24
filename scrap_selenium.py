331df6b877130bfcdddbb6363204b80150e395a7
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-05-24 13:46:38 -0500

    Use executable path for windows in webdriver chrome

--- scrap_selenium.py
+++ scrap_selenium.py
@@ -8,18 +8,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 url = 'https://www.latamairlines.com/mx/es'
 
-r = requests.get(url)
+# r = requests.get(url)
 # print(r.status_code)
-
-s = BeautifulSoup(r.text, 'lxml')
+# 
+# s = BeautifulSoup(r.text, 'lxml')
 # print(s.prettify())
 
 options = Options()
 options.add_experimental_option('detach', True)
 options.add_argument('start-maximized')
 options.add_argument('--incognito')
-driver = webdriver.Chrome(chrome_options=options,
-                          service=Service(ChromeDriverManager().install()))
+
+
+driver = webdriver.Chrome(executable_path=r'..\chromedriver.exe')  # for windows
+
+# driver = webdriver.Chrome(chrome_options=options,
+#                           service=Service(ChromeDriverManager().install()))  # for linux
 driver.get(url)
 
 # driver.close()
