e5767ec144464325d074b366369e4483c5768100
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-05-27 16:11:32 -0500

    creating function for getting information about stops

--- scrap_selenium.py
+++ scrap_selenium.py
@@ -47,7 +47,7 @@ print(itinerary_modal)
 # %%
 itinerary_modal.click()
 # %%
-legs = driver.find_elements(By.XPATH, '//section[@data-test="section-info-leg"]')
+legs = flight.find_elements(By.XPATH, '//section[@data-test="section-info-leg"]')
 print(f'Number of legs: {len(legs)}')
 # %%
 num_stops = len(legs) - 1
@@ -79,7 +79,7 @@ print(leg.find_element(By.XPATH, '//span[@class="pathInfo-destination incoming-o
 # Arrival Time Leg
 print(leg.find_element(By.XPATH, '//span[@class="pathInfo-destination incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[2]').text.replace('. ', '.'))
 # %%
-connections = driver.find_elements(By.XPATH, '//section[@data-test="section-info-connection"]')
+connections = leg.find_elements(By.XPATH, '//section[@data-test="section-info-connection"]')
 print(f'Number of connections: {len(connections)}')
 
 # Delay Connections
@@ -87,7 +87,59 @@ print(connections[0].find_element(By.XPATH, '//div[@class="sc-kGsDXJ dZJGGL"]/sp
 # %% [markdown]
 # ## Close Modal
 # %%
-button_close = driver.find_element(By.XPATH, '//button[@class="MuiButtonBase-root MuiIconButton-root sc-jbKcbu eQFcRm"]')
+button_close = leg.find_element(By.XPATH, '//button[@class="MuiButtonBase-root MuiIconButton-root sc-jbKcbu eQFcRm"]')
 
 button_close.click()
 # %%
+# open fares in a flight
+# flight.find_element(By.XPATH, './/div[@role="button"]').click()
+flight.click()
+# %%
+fares = flight.find_elements(By.XPATH, './/li[contains(@id, "WrapperBundleCardbundle-detail")]')
+# %%
+
+
+# fare amount
+for fare in fares: print(fare.find_element(By.XPATH, './/span[@class="sc-iipuKH ibJrcp displayAmount"]').text)
+# %%
+# fare type
+for fare in fares: print(fare.find_element(By.XPATH, './/span[@class="sc-eQPCt kXXugL"]').text)
+# %%
+fares_info = {fare.find_element(By.XPATH, './/span[@class="sc-eQPCt kXXugL"]').text: fare.find_element(By.XPATH, './/span[@class="sc-iipuKH ibJrcp displayAmount"]').text for fare in fares}
+
+print(fares_info)
+#%%
+# close modal
+flight.find_element(By.XPATH, './/button[contains(@class,"MuiButtonBase-root")]').click()
+# %%
+def get_prices(flight):
+    fares = flight.find_elements(By.XPATH, './/li[contains(@id, "WrapperBundleCardbundle-detail")]')
+    return {fare.find_element(By.XPATH, './/span[@class="sc-eQPCt kXXugL"]').text: fare.find_element(By.XPATH, './/span[@class="sc-iipuKH ibJrcp displayAmount"]').text for fare in fares}
+#%%
+def get_info_leg(flight):
+    'Function that returns information about a leg flight'
+    info_leg = []
+    legs = flight.find_elements(By.XPATH, '//section[@data-test="section-info-leg"]')
+    for leg in legs:
+        # Departure City Leg
+        leg.find_element(By.XPATH, '//span[@class="pathInfo-origin incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[1]').text
+
+        # Departure Time Leg
+        leg.find_element(By.XPATH, '//span[@class="pathInfo-origin incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[2]').text
+
+        # Duration flight
+        leg.find_element(By.XPATH, './/div[@class="sc-cdQEHs jryrRi"]//span[@class="time"]').text
+
+        # airplane model
+        leg.find_element(By.XPATH, './/div[@class="sc-bNpCPZ fOmMxC"]/span[@class="airplane-code"]').text
+
+        # flight number
+        leg.find_element(By.XPATH, '//div[@class="incoming-outcoming-title"]/div').text
+        # %%
+        # Arrival City Leg
+        print(leg.find_element(By.XPATH, '//span[@class="pathInfo-destination incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[1]').text)
+
+        # Arrival Time Leg
+        print(leg.find_element(By.XPATH, '//span[@class="pathInfo-destination incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[2]').text.replace('. ', '.'))
+
+    return info_leg
