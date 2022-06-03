c192c8ebce0fdbf3a0647be2fc236754d2d85f38
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-06-02 19:03:32 -0500

    function to get prices

--- scrap_selenium.py
+++ scrap_selenium.py
@@ -31,12 +31,13 @@ print(flight)
 
 # %%
 departure = flight.find_element(
-    By.XPATH, './/div[@class="sc-kBMPsl hKrdMC flight-information"][1]/span').text
+    By.XPATH, './/div[contains(@class, "flight-information")][1]/span').text
+
 arrival = flight.find_element(
-    By.XPATH, './/div[@class="sc-kBMPsl hKrdMC flight-information"][2]/span').text
+    By.XPATH, './/div[contains(@class, "flight-information")][2]/span').text
 
 duration = flight.find_element(
-    By.XPATH, './/div[@class="sc-kBMPsl hKrdMC flight-duration"]/span[2]').text
+    By.XPATH, './/div[contains(@class, "flight-duration")]/span[2]').text
 
 print(departure)
 print(arrival)
@@ -61,29 +62,37 @@ leg = legs[0]
 # Departure City Leg
 print(leg.find_element(By.XPATH, '//span[@class="pathInfo-origin incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[1]').text)
 
+#%%
 # Departure Time Leg
 print(leg.find_element(By.XPATH, '//span[@class="pathInfo-origin incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[2]').text)
-
+#%%
 # Duration flight
-print(leg.find_element(By.XPATH, './/div[@class="sc-cdQEHs jryrRi"]//span[@class="time"]').text)
+print('Flight Duration')
+print(leg.find_element(By.XPATH, './/span[@class="pathInfo-duration"]/following-sibling::span[@class="time"]').text)
 
+#%%
 # airplane model
-print(leg.find_element(By.XPATH, './/div[@class="sc-bNpCPZ fOmMxC"]/span[@class="airplane-code"]').text)
-
+print('Airplane Code:')
+print(leg.find_element(By.XPATH, './/span[@class="airplane-code"]').text)
+# %%
 # flight number
+print('Flight Number:')
 print(leg.find_element(By.XPATH, '//div[@class="incoming-outcoming-title"]/div').text)
 # %%
 # Arrival City Leg
+print('Arrival City Leg')
 print(leg.find_element(By.XPATH, '//span[@class="pathInfo-destination incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[1]').text)
-
+#%%
 # Arrival Time Leg
+print('Arrival Time for the Leg')
 print(leg.find_element(By.XPATH, '//span[@class="pathInfo-destination incoming-outcoming-title"]/following-sibling::div[@class="iataCode"]/span[2]').text.replace('. ', '.'))
 # %%
 connections = leg.find_elements(By.XPATH, '//section[@data-test="section-info-connection"]')
 print(f'Number of connections: {len(connections)}')
-
+#%%
 # Delay Connections
-print(connections[0].find_element(By.XPATH, '//div[@class="sc-kGsDXJ dZJGGL"]/span[@class="time"]').text)
+print('Delay of the connection:')
+print(connections[0].find_element(By.XPATH, './/section//span[@class="time"]').text)
 # %% [markdown]
 # ## Close Modal
 # %%
@@ -98,27 +107,33 @@ flight.click()
 fares = flight.find_elements(By.XPATH, './/li[contains(@id, "WrapperBundleCardbundle-detail")]')
 # %%
 
-
 # fare amount
-for fare in fares: print(fare.find_element(By.XPATH, './/span[@class="sc-iipuKH ibJrcp displayAmount"]').text)
+fare_amounts = [fare.find_element(By.XPATH, './/span[contains(@class, "displayAmount")]').text for fare in fares]
+print(fare_amounts)
 # %%
 # fare type
-for fare in fares: print(fare.find_element(By.XPATH, './/span[@class="sc-eQPCt kXXugL"]').text)
+print('Fare Types:')
+fare_types = [fare.find_element(By.XPATH, './div/div/div/div/span[@title]').text for fare in fares]
+print(fare_types)
 # %%
-fares_info = {fare.find_element(By.XPATH, './/span[@class="sc-eQPCt kXXugL"]').text: fare.find_element(By.XPATH, './/span[@class="sc-iipuKH ibJrcp displayAmount"]').text for fare in fares}
+print('Fares Info:')
 
+fares_info = dict(zip(fare_types, fare_amounts))
 print(fares_info)
+
 #%%
 # close modal
 flight.find_element(By.XPATH, './/button[contains(@class,"MuiButtonBase-root")]').click()
 # %%
-def get_prices(flight):
+def get_prices(flight):    
     fares = flight.find_elements(By.XPATH, './/li[contains(@id, "WrapperBundleCardbundle-detail")]')
-    return {fare.find_element(By.XPATH, './/span[@class="sc-eQPCt kXXugL"]').text: fare.find_element(By.XPATH, './/span[@class="sc-iipuKH ibJrcp displayAmount"]').text for fare in fares}
+    fare_types = [fare.find_element(By.XPATH, './div/div/div/div/span[@title]').text for fare in fares]
+    fare_amounts = [fare.find_element(By.XPATH, './/span[contains(@class, "displayAmount")]').text for fare in fares]
+    return dict(zip(fare_types, fare_amounts))
 #%%
 def get_info_leg(flight):
     'Function that returns information about a leg flight'
-    info_leg = []
+    info_legs = []    
     legs = flight.find_elements(By.XPATH, '//section[@data-test="section-info-leg"]')
     for leg in legs:
         # Departure City Leg
